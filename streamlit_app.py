74efdd8d6489203700b3c52434052f4225de1bc3
riteshsnowflake <131040918+riteshsnowflake@users.noreply.github.com>
2023-04-17 15:43:13 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ streamlit.title('My Parents New healthy Diner')
 
 streamlit.title('Breakfast menu')
 
-streamlit.title('Omega 3 & Blueberry Oatmeal')
-streamlit.title('Kale, Spinach & Rocket Smoothie')
-streamlit.title('hard-Boiled Free-Range Egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('hard-Boiled Free-Range Egg')
